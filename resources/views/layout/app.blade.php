5be80a31a47f49522e07df1f6df92281672d9956
Baptistte <baptistegrincourt@gmail.com>
2025-01-15 14:36:56 +0100

    changement global : graphisme et ajout icone..

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,9 +1,11 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  <title>Quentix</title>
+  <link rel="icon" href="{{ asset('images/quentix-logo.png') }}" type="image/png">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
   <h1 class="text-3xl font-bold underline">
